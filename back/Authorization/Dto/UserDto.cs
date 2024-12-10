ca347145efcd54671d17f805d3ee85f61e952610
Mixail <mixail.chernyshev.00@gmail.com>
2024-12-10 22:09:15 +0500

    Все проекты в одном sln

---
+++ back/Authorization/Dto/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MyMvcApp.DTO
+{
+    public class UserDto
+    {
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
