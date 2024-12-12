e77302f267051775674edb7f4aa25f15bc56f6d1
Dmitriy Kopeikin <d.kopeikin@bright-soft.ru>
2024-12-13 00:13:54 +0500

    добавил авторизацию, добавил провайдера имени юзера, который сделал запрос

--- back/SCT.Users/Program.cs
+++ back/SCT.Users/Program.cs
@@ -1,18 +1,64 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using SCT.Common.Data.DatabaseContext;
+using SCT.Users.Providers;
 using SCT.Users.Repositories;
 using SCT.Users.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DatabaseContext>(options =>
-                                                   options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))); // Настройка подключения к базе данных PostgreSQL
-builder.Services.AddScoped<UserRepository>(); // Регистрация репозитория
-builder.Services.AddScoped<UserService>();    // Регистрация сервиса
-builder.Services.AddControllers();
+                                                   options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("SCT.Users")), ServiceLifetime.Singleton, ServiceLifetime.Singleton); // Настройка подключения к базе данных PostgreSQL
+builder.Services.AddSingleton<UserRepository>(); // Регистрация репозитория
+builder.Services.AddSingleton<UserService>();    // Регистрация сервиса
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<IUsernameProvider, UsernameProvider>();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(new Microsoft.AspNetCore.Mvc.Authorization.AuthorizeFilter());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = "Введите JWT токен следующим образом: Bearer {токен}",
+        Name = "Authorization",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
+    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
+
+builder.Services.AddAuthentication(options =>
+       {
+           options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+           options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+       })
+       .AddJwtBearer(options =>
+       {
+           options.Authority = builder.Configuration["Jwt:Authority"];
+           options.Audience = builder.Configuration["Jwt:Audience"];
+           options.RequireHttpsMetadata = false; // Используйте true для продакшн-среды
+           options.IncludeErrorDetails = true;
+       });
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -32,6 +78,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
